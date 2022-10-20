4f1bdd310eee3e473d5c2b2966a6337ba99f95ae
Noquepo <bryanjue1998@gmail.com>
2022-10-20 10:03:32 +0200

    bug fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { play } from "./module/botonFunctions";
+import { play } from "./module/botonFunctions.js";
 let userInput = prompt("1 reproduce la musica");
 export function sound() {if (parseInt(userInput) === 1) {
   play();
