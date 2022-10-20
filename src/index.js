be419a7d4c7e02c029928ce573f8b2d3f1425e36
Noquepo <bryanjue1998@gmail.com>
2022-10-20 14:28:12 +0200

    sound works

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { play } from "./module/botonFunctions.js";
 let userInput = prompt("1 reproduce la musica");
-export function sound() {if (parseInt(userInput) === 1) {
+if (parseInt(userInput) === 1) {
   play();
-}
 }
