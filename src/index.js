c18baa8d8a07747c4054d1d175ba897fe62d854f
Noquepo <noquepo@users.noreply.github.com>
2022-10-22 14:25:14 +0200

    primera implementacion del DOM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-import { play, pause } from "./module/botonFunctions.js";
-const userInput = prompt("1 reproduce la musica");
-if (parseInt(userInput) === 1) {
-  play.then(play());
-}
-const userInput1 = prompt("2 pausa la musica");
-if (parseInt(userInput1) === 2) {
-  pause.then(pause());
-}
+import { playPause } from "./module/botonFunctions.js";
+buttonplay.addEventListener("click", playPause);
--- src/module/botonFunctions.js
+++ src/module/botonFunctions.js
@@ -1,24 +1,27 @@
 import { Howl } from "howler";
 const url = "https://cdn.pixabay.com/download/audio/2022/05/17/audio_581ce59413.mp3?filename=metal-dark-matter-111451.mp3";
+
 class Song {
   constructor(title, img, url) {
     this.title = title;
     this.img = img;
     this.url = url;
-    const music = new Howl({
-      src: [metal.url]
+    this.music = new Howl({
+      src: [this.url]
     });
-    function play() {
-      return music.play();
-    }
   }
 }
-export function pause() {
-  music.pause();
-}
-const music = new Howl({
+const cancion = new Howl({
   src: [url]
 });
-export function play() {
-  return music.play();
+const music1 = new Song("hola", "hola", url);
+let musicController = 1;
+export function playPause() {
+  if (musicController === 1) {
+    music1.music.play();
+    musicController = ++musicController;
+  } else if (musicController === 2) {
+    music1.music.pause();
+    musicController = 1;
+  }
 }
